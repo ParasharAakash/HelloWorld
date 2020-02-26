430c13799630b3933ea0ef1967db5c71eb9c35cf
ParasharAakash <aakash.parashar@mail.vinove.com>
2020-02-26 16:39:09 +0530

    changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{ReactiveFormsModule} from '@angular/forms';
+//
+//
+  
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-@NgModule({
-  declarations: [
-    AppComponent
+import { TestComponent } from './test/test.component';
+import { AakashComponent } from './aakash/aakash.component';
+                                                           
+@NgModule({        
+  declarations: [          
+    AppComponent,                            
+    TestComponent,                                                                     
+    AakashComponent
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
